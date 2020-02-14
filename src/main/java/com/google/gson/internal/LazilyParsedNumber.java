90875ae2fd966f0fd8e71fbd8bbb617539cd2f2d
Massimiliano Menarini <mmenarini@ucsd.edu>
2020-02-14 02:55:17 -0800

    Testing new feature of plugin

--- src/main/java/com/google/gson/internal/LazilyParsedNumber.java
+++ src/main/java/com/google/gson/internal/LazilyParsedNumber.java
@@ -50,7 +50,7 @@ public final class LazilyParsedNumber extends Number {
     try {
       return Long.parseLong(value);
     } catch (NumberFormatException e) {
-      return new BigDecimal(value).longValue();
+      return new BigInteger(value).longValue();
     }
   }
 
